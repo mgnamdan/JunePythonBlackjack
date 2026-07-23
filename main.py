5c6913bd25409ebf42129e85a3389596a5b45ae6
mgnamdan <danny.ai.hub@gmail.com>
2026-07-22 20:20:09 -0400

    finish blackjack

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # HELPER FUNCTIONS AND IMPORTS
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-from cards import Card
+from manager import BlackjackManager
 
 
 
@@ -10,33 +10,9 @@ from cards import Card
 # MAIN FUNCTION DEFINITION
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def main():
-    testDeck = Deck()
-    testPlayer = CompBJPlayer()
-    testPlayer2 = HumanBJPlayer()
+    game = BlackjackManager()
 
-    card1 = Card("Jack", "Hearts")
-    card2 = Card("Eight", "Spades")
-    card3 = Card("Seven", "Clubs")
-    card4 = Card()
-    card5 = Card()
-    card6 = Card()
-
-    testPlayer2.drawCard(card1)
-    testPlayer2.drawCard(card2)
-
-    print(f"choice = {testPlayer2.makeChoice()}")
-
-    testPlayer2.drawCard(card3)
-
-    print(f"choice = {testPlayer2.makeChoice()}")
-
-    testPlayer2.discardCard(0)
-
-    testPlayer2.drawCard(card4)
-    testPlayer2.drawCard(card5)
-    testPlayer2.drawCard(card6)
-
-    print(f"choice = {testPlayer2.makeChoice()}")
+    game.playGame()
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- manager.py
+++ manager.py
@@ -7,7 +7,8 @@ class BlackjackManager:
     COMPNAMES = ["Adam", "Bob", "Cassie", "Doug", "Elizabeth", "Fred", "Gabby"]
 
     def __init__(self):
-        self.reset()
+        self.playGame()
+
 
 
     def reset(self):
@@ -37,15 +38,24 @@ class BlackjackManager:
         except ValueError:
             numComps = 0
 
+        usedNames = []
+
         for _ in range(numComps):
-            newPlayer = CompBJPlayer(choice(self.COMPNAMES))
-            self.players.append(newPlayer)
+            validName = False
+            while not validName:
+                newName = choice(self.COMPNAMES)
+                if newName not in usedNames:
+                    usedNames.append(newName)
+                    newPlayer = CompBJPlayer(newName)
+                    self.players.append(newPlayer)
+                    validName = True
 
         self.players.append(self.dealer)
 
         for _ in range(2):
             for player in self.players:
                 player.drawCard(self.deck.draw())
+
 
 
     def manageTurn(self, player):
@@ -56,13 +66,76 @@ class BlackjackManager:
                 player.drawCard(self.deck.draw())
     
 
-    def determineWinner(self):
-        pass
-
 
     def promptNextGame(self):
-        pass
+        validChoice = False
+        while not validChoice:
+            print("")
+            print("Would you like to play another game? (y/n)")
+            choice = input(" --> ").lower()
+            if choice in ["yes", "y"]:
+                choice = True
+                validChoice = True
+            elif choice in ["no", "n", "quit", "exit"]:
+                choice = False
+                validChoice = True
+            else:
+                print("")
+                print("Invalid choice - please try again!")
+        return choice
+
+
+
+    def determineWinner(self):
+        # Dealer has 21 -> dealer wins; beats everyone
+        # Anyone else has 21 -> they win (ties can happen)
+        # Nobody has 21:
+        #     1 high score -> they win
+        #     multiple high scores -> they tie or dealer wins if present
+        # Everyone busts -> nobody wins
+        scores = {}
+        for player in self.players:
+            score = player.giveScore()
+            if score <= 21:
+                if score in scores.keys():
+                    scores[score].append(player.name)
+                else:
+                    scores[score] = [player.name]
+
+        scoreList = []
+        for pts in scores.keys():
+            scoreList.append(pts)
+        scoreList.sort()
+        highScore = scoreList[-1]
+        winners = scores[highScore]
+
+        if len(winners) == 0:
+            print("")
+            print("Everyone busts! Everyone loses!")
+        elif len(winners) == 1:
+            print("")
+            print(f"Congratulations {winners[0]}! You win with a score of {highScore}!")
+        else:
+            if self.dealer in winners:
+                print("")
+                print(f"The dealer wins with a score of {highScore}!")
+            else:
+                print("")
+                message = ""
+                for idx in range(len(winners)):
+                    if idx == len(winners) - 1:
+                        message += f" and {player.name} win with a score of {highScore}!"
+                    else: 
+                        message += f"{player.name}, "
+                print(message)
+
 
 
     def playGame(self):
-        pass
+        gameOn = True
+        while gameOn:
+            self.reset()
+            for player in self.players:
+                self.manageTurn(player)
+            self.determineWinner()
+            gameOn = self.promptNextGame()
